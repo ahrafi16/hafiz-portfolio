21aa5aa9f26bcdcdc0b06ee15ebebac29a0355ed
ahrafi16 <anjumhossainrafi16@gmail.com>
2025-10-30 02:49:43 +0600

    Added link

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import { TbBrandGithub } from "react-icons/tb";
 import { TbBrandLinkedin } from "react-icons/tb";
 import { FaXTwitter } from "react-icons/fa6";
-import { GoTag } from "react-icons/go";
+import { LuTag } from "react-icons/lu";
 import { FaRegStar } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa6";
 import Image from "next/image";
@@ -15,8 +15,7 @@ export default function Home() {
       <div className="max-w-3xl">
         <h2 className="text-4xl my-5 font-bold">Hey! I&#39;m <span className="text-[#a6e3a1]">Hafizur Rahman</span> </h2>
         <p className="text-xl leading-8">
-          A student building and securing web infrastructure. I&#39;ve written software that is trusted by The United Nations, The Linux Foundation, Arch Linux, GNOME, Wine, FFmpeg and many others[1]. My code is used by millions of people worldwide.
-          I focus on backend web development, scalable and distributed systems, DevOps, and cybersecurity, especially crafting defenses against automated threats. I enjoy leading initiatives and tackling complex technical challenges.
+          I am a final-year BSc Computer Science student with a passion for mobile app development. I started my journey learning Dart and Flutter, building several exciting applications along the way. Recently, I have been diving into Swift, and I find iOS development increasingly fascinating. I love coding, creating apps, and solving problems through programming. From the beginning, I have been drawn to building intuitive mobile experiences, sometimes integrating Laravel as a backend for my applications. I am continuously learning, exploring new technologies, and improving my skills to create impactful apps.
         </p>
       </div>
       <div className="my-5 flex items-center gap-6">
@@ -38,14 +37,14 @@ export default function Home() {
 
         <span className="text-xs text-gray-600">|</span>
 
-        <a className="hover:text-[#a6e3a1]" href="https://www.facebook.com/hafizur.rahman.68430" target="_blank">
+        <a className="hover:text-[#a6e3a1]" href="https://x.com/hafizflow" target="_blank">
           <span className="flex items-center gap-2"><FaXTwitter className="text-lg" />
           </span>
         </a>
       </div>
 
       <div className="flex justify-between items-center mt-20 mb-10">
-        <h1 className="text-4xl flex  gap-2 items-center"><FaRegStar className="text-[#a6e3a1] text-2xl" />Featured Projects</h1>
+        <h1 className="text-4xl flex font-semibold  gap-2 items-center"><FaRegStar className="text-[#a6e3a1] text-2xl" />Featured Projects</h1>
         <p className="flex items-center gap-1 text-[#a6e3a1] cursor-pointer">view all <FaArrowRight /></p>
       </div>
       <div className="flex flex-col md:flex-row gap-7">
@@ -64,13 +63,16 @@ export default function Home() {
             </div>
           </div>
           <div className="space-y-3 p-5">
-            <h2 className="text-xl font-semibold group-hover:text-[#a6e3a1] transition-colors duration-300">DIU Routine</h2>
+            <h2 className="text-xl flex justify-between items-center font-semibold group-hover:text-[#a6e3a1] transition-colors duration-300">
+              <span>DIU Routine</span>
+              <TbBrandGithub className="" />
+            </h2>
             <p>DIU Routine is a modern iOS app designed to help students and teachers of Daffodil International University easily access class routines and campus room information. It offers a clean UI, smooth performance, and real-time updates to make academic scheduling simple and efficient.</p>
             <div className="flex items-center gap-2">
-              <GoTag />
+              <LuTag />
               <div className="flex gap-1">
                 <p className="text-blue-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">Swift</p>
-                <p className="text-green-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">Swift UI</p>
+                <p className="text-green-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">SwiftUI</p>
                 <p className="text-yellow-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">SwiftData</p>
                 <p className="text-red-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">PDFkit</p>
                 <p className="text-orange-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">Laravel</p>
@@ -92,14 +94,17 @@ export default function Home() {
             </div>
           </div>
           <div className="space-y-3 p-5">
-            <h2 className="text-xl font-semibold group-hover:text-[#a6e3a1] transition-colors duration-300">GradeX</h2>
+            <h2 className="text-xl flex justify-between items-center font-semibold group-hover:text-[#a6e3a1] transition-colors duration-300">
+              <span>GradeX</span>
+              <TbBrandGithub className="" />
+            </h2>
             <p>GradeX is a powerful and intuitive iOS app built with Swift that helps students calculate and manage their CGPA with ease. The app allows users to input GPA and credits for each course, automatically computing the overall CGPA with accurate results.
             </p>
             <div className="flex items-center gap-2">
-              <GoTag />
+              <LuTag />
               <div className="flex gap-1">
                 <p className="text-blue-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">Swift</p>
-                <p className="text-green-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">Swift UI</p>
+                <p className="text-green-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">SwiftUI</p>
                 <p className="text-yellow-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">SwiftData</p>
                 <p className="text-red-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">PDFkit</p>
               </div>
--- app/project/page.js
+++ app/project/page.js
@@ -1,100 +1,165 @@
 import { MdOutlineFolderCopy } from "react-icons/md";
-import { GoTag } from "react-icons/go";
+import { LuTag } from "react-icons/lu";
+import { FaAppStore } from "react-icons/fa";
+import Image from "next/image";
+import p1 from "../../public/project1.jpg";
+import p3 from "../../public/project3.jpg";
+import p4 from "../../public/project4.jpg";
+import p5 from "../../public/project5.jpg";
+import p6 from "../../public/project6.jpg";
+import { TbBrandGithub } from "react-icons/tb";
 
 const page = () => {
 
     const projects = [
         {
             id: 1,
-            code: 13400,
-            name: "DeepVision AI",
-            version: "mara 1",
+            code: p1,
+            name: "DIU Routine",
+            version: "https://github.com/hafizflow/DiuRoutine",
             description:
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat sapiente numquam laborum praesentium nostrum delectus, nam mollitia sunt neque?",
-            contributors: 137,
-            title: "Prohgfjsk khgf",
+                "DIU Routine is a modern iOS app designed to help students and teachers of Daffodil International University easily access class routines and campus room information. It offers a clean UI, smooth performance, and real-time updates to make academic scheduling simple and efficient.",
+            contributors: "https://apps.apple.com/us/app/diu-routine/id6748752277",
+            title: "DIU Routine",
             details:
-                "Developed using Random Forest and XGBoost, trained on UCI dataset with 85% precision.",
-            tags: ["react", "tailwind", "nextjs", "mongodb"],
+                "DIU Routine is a modern iOS app designed to help students and teachers of Daffodil International University easily access class routines and campus room information. It offers a clean UI, smooth performance, and real-time updates to make academic scheduling simple and efficient.",
+            tags: ["Swift", "SwiftUI", "Swift Data", "PDFKit"],
         },
         {
             id: 2,
-            code: 10291,
-            name: "DeepVision AI",
-            version: "mara 1",
+            code: p1,
+            name: "GradeX ",
+            version: "https://github.com/hafizflow/GradeX",
             description:
                 "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat sapiente numquam laborum praesentium nostrum delectus, nam mollitia sunt neque?",
             contributors: 52,
-            title: "Deepfake Detection Model",
+            title: "GradeX",
             details:
-                "Developed using Random Forest and XGBoost, trained on UCI dataset with 85% precision.",
-             tags: ["react", "tailwind", "nextjs", "mongodb"],
+                "GradeX is a powerful and intuitive iOS app built with Swift that helps students calculate and manage their CGPA with ease. The app allows users to input GPA and credits for each course, automatically computing the overall CGPA with accurate results.",
+            tags: ["Swift", "SwiftUI", "Swift Data", "PDFKit"],
         },
         {
             id: 3,
-            code: 98324,
-            name: "DeepVision AI",
-            version: "mara 1",
+            code: p3,
+            name: "Cover Crafter",
+            version: "https://github.com/hafizflow/Cover-Crafter",
             description:
                 "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat sapiente numquam laborum praesentium nostrum delectus, nam mollitia sunt neque?",
             contributors: 88,
-            title: "Heart Disease Prediction",
+            title: "Cover Crafter",
             details:
-                "Developed using Random Forest and XGBoost, trained on UCI dataset with 85% precision.",
-             tags: ["react", "tailwind", "nextjs", "mongodb"],
+                "Cover Crafter is a Flutter-based mobile application designed to help students effortlessly create beautiful and professional university assignment and lab report cover pages in seconds",
+            tags: ["Flutter", "Dart", "GetX", "MVC"],
+        },
+        {
+            id: 4,
+            code: p4,
+            name: "Cover Crafter",
+            version: "https://github.com/hafizflow/DIU-Result.",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat sapiente numquam laborum praesentium nostrum delectus, nam mollitia sunt neque?",
+            contributors: 88,
+            title: "DIU Result",
+            details:
+                "The DIU Result App is a Flutter-based mobile application designed for Daffodil International University (DIU) students to easily access and view their academic results. The app provides a user-friendly interface and a seamless experience to check semester-wise results and overall CGPA.",
+            tags: ["Flutter", "Dart", "GetX", "MVC", "API"],
+        },
+        {
+            id: 5,
+            code: p5,
+            name: "We Chat",
+            version: "https://github.com/hafizflow/We-Chat",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat sapiente numquam laborum praesentium nostrum delectus, nam mollitia sunt neque?",
+            contributors: 88,
+            title: "We Chat",
+            details:
+                "A real-time chat application built with Flutter and Firebase, offering push notifications, Google Sign-In, and editable user profiles with photos, last-seen, and about info. It supports message read status, deleting & editing messages, copying text, sharing multiple images, saving images to the gallery, member search, and comes with a clean, modern Material UI for a seamless chatting experience.",
+            tags: ["Flutter", "Dart", "GetX", "Firebase"],
+        },
+        {
+            id: 6,
+            code: p6,
+            name: "We Chat",
+            version: "https://github.com/hafizflow/Infinity-Buy",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quaerat sapiente numquam laborum praesentium nostrum delectus, nam mollitia sunt neque?",
+            contributors: 88,
+            title: "Infinity Buy",
+            details:
+                "A Flutter-based e-commerce app with a clean architecture and GetX for state management. Features include product browsing, search, filtering, and sorting with dynamic data from REST APIs. The app offers a modern, responsive UI, smooth navigation, optimized performance, and efficient API integration for a seamless shopping experience",
+            tags: ["Flutter", "Dart", "GetX", "Firebase"],
         },
     ];
 
     return (
         <div className="">
-            <h2 className="flex items-center text-4xl font-bold gap-2 my-5"><MdOutlineFolderCopy className="text-[#a6e3a1]" />Projects</h2>
+            <h2 className="flex items-center  text-4xl font-semibold gap-2 my-7"><MdOutlineFolderCopy className="text-[#a6e3a1]" />Projects</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {projects.map((project) => (
                     <div
                         key={project.id}
-                        className="group border border-gray-700 hover:border-[#a6e3a1] rounded-2xl w-full cursor-pointer"
+                        className="group border border-gray-700 hover:border-[#a6e3a1] rounded-2xl w-full h-full flex flex-col cursor-pointer"
                     >
                         {/* Top box */}
                         <div className="bg-[#a9b4c0] p-7 rounded-t-2xl">
-                            <div className="bg-[#282a37] transition-transform duration-300 group-hover:scale-105 space-y-5 rounded-xl p-3 shadow-2xl">
-                                <div className="flex justify-between items-center">
-                                    <div className="flex gap-1">
-                                        <div className="bg-red-600 w-3 h-3 rounded-full"></div>
-                                        <div className="bg-yellow-600 w-3 h-3 rounded-full"></div>
-                                        <div className="bg-green-600 w-3 h-3 rounded-full"></div>
-                                    </div>
-                                    <div>{project.code}</div>
-                                </div>
-
-                                <div className="text-xl">
-                                    {project.name} / <span>{project.version}</span>
-                                </div>
-                                <div>{project.description}</div>
-                                <div className="flex justify-end">
-                                    <p>{project.contributors} Contributors</p>
-                                </div>
+                            <div className="bg-[#282a37] transition-transform duration-300 group-hover:scale-105 space-y-5 rounded-xl shadow-2xl">
+                                <Image
+                                    src={project.code}
+                                    alt="Picture of the author"
+                                    width={900}
+                                    height={900}
+                                    className="rounded-xl h-45 w-full md:w-[500px] lg:w-[700px]"
+                                />
                             </div>
                         </div>
 
                         {/* Bottom box */}
-                        <div className="space-y-3 p-5">
-                            <h2 className="text-xl font-semibold group-hover:text-[#a6e3a1] transition-colors duration-300">
-                                {project.title}
+                        <div className="flex flex-col gap-3 flex-1 p-5">
+                            <h2 className="text-xl flex justify-between items-center font-semibold group-hover:text-[#a6e3a1] transition-colors duration-300">
+                                <span>{project.title}</span>
+                                <span className="flex items-center gap-3">
+                                    {
+                                        project.id == 1 ? <a href={project.contributors} target="_blank" rel="noopener noreferrer">
+                                            <FaAppStore className="text-xl hover:text-[#a6e3a1]" />
+                                        </a> : null
+                                    }
+                                    <a href={project.version} target="_blank" rel="noopener noreferrer">
+                                        <TbBrandGithub className="text-xl hover:text-[#a6e3a1]" />
+                                    </a>
+                                </span>
                             </h2>
-                            <p>{project.details}</p>
+                            <p className="grow">{project.details.length >= 90 ? project.details.slice(0, 90) + "..." : project.details}</p>
+
                             <div className="flex items-center gap-2">
 
-                                <div className="flex items-center gap-1 flex-wrap">
-                                     <GoTag className="text-lg mr-1 font-semibold"/>
-                                    {project.tags.map((tag, index) => (
-                                        <p
-                                            key={index}
-                                            className="text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md text-blue-200"
-                                        >
-                                            {tag}
-                                        </p>
-                                    ))}
+                                <div className="flex items-center gap-1 mt-4 flex-wrap">
+                                    <LuTag className="text-lg mr-1 font-semibold" />
+                                    {project.tags.map((tag, index) => {
+                                        const colorMap = {
+                                            swift: "text-blue-200",
+                                            swiftui: "text-yellow-200",
+                                            mvc: "text-red-200",
+                                            swiftdata: "text-green-200",
+                                            pdfkit: "text-coral-200",
+                                            flutter: "text-pink-200",
+                                            dart: "text-teal-200",
+                                            getx: "text-orange-200",
+                                        };
+
+                                        const color = colorMap[tag.toLowerCase()] || "text-blue-200";
+
+                                        return (
+                                            <p
+                                                key={index}
+                                                className={`text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md ${color}`}
+                                            >
+                                                {tag}
+                                            </p>
+                                        );
+                                    })}
                                 </div>
+
                             </div>
                         </div>
                     </div>
