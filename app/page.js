39735f25bfb0eb7d035bffa4e3d2288473bc5cd1
ahrafi16 <anjumhossainrafi16@gmail.com>
2025-10-30 13:17:01 +0600

    mobile responsive done

--- app/page.js
+++ app/page.js
@@ -16,34 +16,36 @@ export default function Home() {
       <div className="max-w-3xl">
         <h2 className="text-4xl my-5 font-bold">Hey! I&#39;m <span className="text-[#a6e3a1]">Hafizur Rahman</span> </h2>
         <p className="text-xl leading-8">
-          I am a final-year BSc Computer Science student with a passion for mobile app development. I started my journey learning Dart and Flutter, building several exciting applications along the way. Recently, I have been diving into Swift, and I find iOS development increasingly fascinating. I love coding, creating apps, and solving problems through programming. From the beginning, I have been drawn to building intuitive mobile experiences, sometimes integrating Laravel as a backend for my applications. I am continuously learning, exploring new technologies, and improving my skills to create impactful apps.
+          I&#39;m an iOS developer with a strong passion for creating intuitive and user-centered applications using Swift and SwiftUI. I enjoy transforming innovative ideas into real-world digital solutions that make a meaningful impact. Over time, I&#39;ve built several practical apps that solve everyday problems and are actively used by many users. My focus is on writing clean, maintainable code and delivering smooth, visually appealing user experiences that bring ideas to life.
         </p>
       </div>
-      <div className="my-5 flex items-center gap-6">
-        <a className="hover:text-[#a6e3a1]" href="https://github.com/hafizflow" target="_blank">
-          <span className="flex items-center gap-2">
-            <TbBrandGithub className="text-lg" />
-            Github
-          </span>
-        </a>
+      <div className="my-5 flex flex-col md:flex-row items-center gap-6">
+        <div className="flex items-center gap-4 md:gap-6 ">
+          <a className="hover:text-[#a6e3a1]" href="https://github.com/hafizflow" target="_blank">
+            <span className="flex items-center gap-2">
+              <TbBrandGithub className="text-lg" />
+              Github
+            </span>
+          </a>
 
-        <span className="text-xs text-gray-600">|</span>
+          <span className="text-xs text-gray-600">|</span>
 
-        <a className="hover:text-[#a6e3a1]" href="https://www.linkedin.com/in/hafizflow/" target="_blank">
-          <span className="flex items-center gap-2">
-            <TbBrandLinkedin className="text-lg" />
-            LinkedIn
-          </span>
-        </a>
+          <a className="hover:text-[#a6e3a1]" href="https://www.linkedin.com/in/hafizflow/" target="_blank">
+            <span className="flex items-center gap-2">
+              <TbBrandLinkedin className="text-lg" />
+              LinkedIn
+            </span>
+          </a>
 
-        <span className="text-xs text-gray-600">|</span>
+          <span className="text-xs text-gray-600">|</span>
 
-        <a className="hover:text-[#a6e3a1]" href="https://x.com/hafizflow" target="_blank">
-          <span className="flex items-center gap-2"><FaXTwitter className="text-lg" />
-          </span>
-        </a>
+          <a className="hover:text-[#a6e3a1]" href="https://x.com/hafizflow" target="_blank">
+            <span className="flex items-center gap-2"><FaXTwitter className="text-lg" />
+            </span>
+          </a>
 
-        <span className="text-xs text-gray-600">|</span>
+          <span className="text-xs text-gray-600">|</span>
+        </div>
 
         <Link className="hover:text-[#a6e3a1]" href="/about">
           <span className="flex items-center group gap-2">More about me <FaArrowRight className="transition-translate duration-100 group-hover:translate-x-1" />
@@ -51,10 +53,13 @@ export default function Home() {
         </Link>
       </div>
 
-      <div className="flex justify-between items-center mt-20 mb-10">
-        <h1 className="text-4xl flex font-semibold  gap-2 items-center"><FaRegStar className="text-[#a6e3a1] text-2xl" />Featured Projects</h1>
+      <div className="flex flex-col md:flex-row justify-between items-center mt-20 mb-10">
+        <h1 className="text-2xl md:text-4xl flex font-semibold  gap-2 items-center">
+          <FaRegStar className="text-[#a6e3a1] text-2xl" />
+          Featured Projects
+        </h1>
 
-        <Link className="text-[#a6e3a1]" href="/project">
+        <Link className="text-[#a6e3a1] hidden md:block" href="/project">
           <span className="flex items-center group gap-2">View all <FaArrowRight className="transition-translate duration-100 group-hover:translate-x-1" />
           </span>
         </Link>
@@ -93,8 +98,8 @@ export default function Home() {
             </h2>
             <p>DIU Routine is a modern iOS app designed to help students and teachers of Daffodil International University easily access class routines and campus room information. It offers a clean UI, smooth performance, and real-time updates to make academic scheduling simple and efficient.</p>
             <div className="flex items-center mt-4 gap-2">
-              <LuTag />
-              <div className="flex gap-1">
+              <LuTag className="text-lg" />
+              <div className="flex gap-5 md:gap-1 overflow-hidden whitespace-nowrap max-w-full">
                 <p className="text-blue-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">Swift</p>
                 <p className="text-green-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">SwiftUI</p>
                 <p className="text-yellow-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">SwiftData</p>
@@ -131,16 +136,20 @@ export default function Home() {
             <p>GradeX is a powerful and intuitive iOS app built with Swift that helps students calculate and manage their CGPA with ease. The app allows users to input GPA and credits for each course, automatically computing the overall CGPA with accurate results.
             </p>
             <div className="flex items-center mt-4 gap-2">
-              <LuTag />
-              <div className="flex gap-1">
+              <LuTag className="text-lg" />
+              <div className="flex gap-5 md:gap-1  overflow-hidden whitespace-nowrap max-w-full">
                 <p className="text-blue-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">Swift</p>
                 <p className="text-green-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">SwiftUI</p>
                 <p className="text-yellow-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">SwiftData</p>
-                <p className="text-red-200 text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">PDFkit</p>
+                <p className="text-red-200 flex text-xs bg-[#313244] px-2 py-1 font-semibold rounded-md">PDFkit</p>
               </div>
             </div>
           </div>
         </div>
+        <Link className="text-[#a6e3a1] mx-auto block md:hidden" href="/project">
+          <span className="flex items-center group gap-2">View all Projects<FaArrowRight className="transition-translate duration-100 group-hover:translate-x-1" />
+          </span>
+        </Link>
 
       </div>
     </main>
